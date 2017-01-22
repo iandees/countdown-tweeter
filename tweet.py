8e48ff8a967a83509aabecf3084ede1b05fe2700
Ian Dees <ian.dees@gmail.com>
2017-01-21 21:52:04 -0500

    Pick a random message, and only do Twitter auth when we need to

--- tweet.py
+++ tweet.py
@@ -2,20 +2,13 @@ import maya
 import os
 import humanize
 import tweepy
+import random
 
 import logging
 import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 dry_run = os.environ.get('DRY_RUN', 'true').lower() == 'true'
-
-CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
-CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
 the_past_str = os.environ.get('PAST_DATETIME')
 the_past = maya.when(the_past_str).datetime()
@@ -28,13 +21,28 @@ now = maya.now().datetime()
 days_in = (now - the_past).days
 total_days = (the_future - the_past).days
 
-message = "{} of {} days".format(
-    humanize.ordinal(days_in),
-    humanize.intcomma(total_days),
-)
+messages = [
+    "{} of {} days".format(
+        humanize.ordinal(days_in),
+        humanize.intcomma(total_days),
+    ),
+    "Day {} of {}".format(
+        humanize.intcomma(days_in),
+        humanize.intcomma(total_days),
+    ),
+]
+
+message = random.choice(messages)
 
 if dry_run:
     logging.info("In dry run mode. Would have tweeted: %s", message)
 else:
+    consumer_key = os.environ.get('CONSUMER_KEY')
+    consumer_secret = os.environ.get('CONSUMER_SECRET')
+    access_token = os.environ.get('ACCESS_TOKEN')
+    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
     api.update_status(message)
     logging.info("Tweeted for real: %s", message)
